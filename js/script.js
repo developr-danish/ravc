2122bd99b91b07b36dade03c05a8321b5b12ba6b
danish4994 <danish0ahmad@gmail.com>
2024-07-25 13:32:35 +0530

    solar panel website

---
+++ js/script.js
@@ -0,0 +1,17 @@
+gsap.from(".mil-up1" , {
+  opacity: 0,
+  y: 50,
+  scrollTrigger: {
+    trigger: ".mil-up1",
+    start: "top 80%",
+  }
+})
+
+gsap.from(".mil-up" , {
+  opacity: 0,
+  y: 50,
+  scrollTrigger: {
+    trigger: ".mil-up",
+    start: "top 80%",
+  }
+})
